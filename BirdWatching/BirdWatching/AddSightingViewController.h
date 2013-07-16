47c721660f1446086cea26c7831ec8eabdeafdc0
Tomonobu Sato <tomonobu.sato@gmail.com>
2013-07-16 23:37:43 +0900

    P66 新規項目の追加の有効化 - シーン追加のUIの設計 まで実装完了

---
+++ BirdWatching/BirdWatching/AddSightingViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddSightingViewController.h
+//  BirdWatching
+//
+//  Created by 佐藤 友信 on 2013/07/16.
+//  Copyright (c) 2013年 Tomonobu Sato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddSightingViewController : UITableViewController
+
+@end
